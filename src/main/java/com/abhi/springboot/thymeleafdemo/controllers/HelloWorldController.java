d97542c14181a3148bd57fbee81a888158e3f3b7
Abhishek <abhishek.km010@gmail.com>
2024-10-09 15:51:41 +0530

    process form version two done

--- src/main/java/com/abhi/springboot/thymeleafdemo/controllers/HelloWorldController.java
+++ src/main/java/com/abhi/springboot/thymeleafdemo/controllers/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.abhi.springboot.thymeleafdemo.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -16,5 +19,33 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm(){
         return "helloworld-process" ;
+    }
+
+    // need a controller method to read form data and
+    // add data to the model.
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from tht HTML form
+
+        String theName = request.getParameter("studentName");
+
+        // convert the name in Upper case
+
+        theName = theName.toUpperCase() ;
+
+        String result = "Yo !" + theName ;
+
+        // add the message to the model
+
+        model.addAttribute("message", result) ;
+        return "helloworld-2";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String letsShoutDudeVersionTwo(@RequestParam("studentName") String name, Model model){
+        name = name.toUpperCase() ;
+        model.addAttribute("message", name ) ;
+        return "helloworld-2" ;
     }
 }
