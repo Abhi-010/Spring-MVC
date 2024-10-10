4dab17109ce28077d498a87cdc2947c0b88f5684
Abhishek <abhishek.km010@gmail.com>
2024-10-10 19:23:40 +0530

    drop down,radio button, check box done

--- src/main/java/com/abhi/springboot/thymeleafdemo/controllers/StudentController.java
+++ src/main/java/com/abhi/springboot/thymeleafdemo/controllers/StudentController.java
@@ -2,14 +2,26 @@ package com.abhi.springboot.thymeleafdemo.controllers;
 
 import com.abhi.springboot.thymeleafdemo.models.Student;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    List<String> countries;
+
+    @Value("${languages}")
+    List<String> languages ;
+
+    @Value("${operatingSystem}")
+    List<String> operatingSystem ;
 
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model theModel ){
@@ -21,6 +33,18 @@ public class StudentController {
         // add student object to the model attribute
         theModel.addAttribute("student", theStudent);
 
+        // add the list of countries to the model
+
+        theModel.addAttribute("countries", countries) ;
+
+        // add the list of programming languages in the model
+
+        theModel.addAttribute("languages", languages) ;
+
+        // add the list of operating system in the model
+
+        theModel.addAttribute("operatingSystem", operatingSystem);
+
         return "student-form";
     }
 
@@ -28,7 +52,8 @@ public class StudentController {
     public String processStudentForm(@ModelAttribute("student") Student theStudent){
 
         System.out.println("theStudent : " + theStudent.getFirstName() + " "
-        + theStudent.getLastName());
+        + theStudent.getLastName() + theStudent.getCountry()
+        + theStudent.getFavoriteProgrammingLanguage());
 
         return "student-confirmation" ;
     }
--- src/main/java/com/abhi/springboot/thymeleafdemo/models/Student.java
+++ src/main/java/com/abhi/springboot/thymeleafdemo/models/Student.java
@@ -1,14 +1,48 @@
 package com.abhi.springboot.thymeleafdemo.models;
 
+import java.util.List;
+
 public class Student {
     private String firstName ;
     private String lastName ;
+    private String country ;
+    private String favoriteProgrammingLanguage ;
+    private List<String> favOperatingSystem ;
 
-    public Student(){}
 
-    public Student(String firstName, String lastName) {
+    public Student(){
+    }
+
+    public Student(String firstName, String lastName, String country, String favoriteProgrammingLanguage, List<String> favOperatingSystem) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.country = country ;
+        this.favoriteProgrammingLanguage = favoriteProgrammingLanguage;
+        this.favOperatingSystem = favOperatingSystem;
+    }
+
+    public List<String> getFavOperatingSystem() {
+        return favOperatingSystem;
+    }
+
+    public void setFavOperatingSystem(List<String> favOperatingSystem) {
+        this.favOperatingSystem = favOperatingSystem;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteProgrammingLanguage() {
+        return favoriteProgrammingLanguage;
+    }
+
+    public void setFavoriteProgrammingLanguage(String favoriteProgrammingLanguage) {
+        this.favoriteProgrammingLanguage = favoriteProgrammingLanguage;
     }
 
     public String getFirstName() {
